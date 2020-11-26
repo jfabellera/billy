134571c5117b5ab7056c28693f9b6d7a3a331869
Jan-Felix Abellera <jfabellera@gmail.com>
2020-11-26 14:21:43 -0600

    adds registration basic back end

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
+var bcrypt = require('bcrypt');
 var router = express.Router();
 
 var monk = require('monk');
-// var db = monk('localhost:27017/billy');
+var db = monk('localhost:27017/billy');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,4 +22,27 @@ router.get('/register', function(req, res) {
   res.render('register');
 });
 
+router.post('/register', async (req, res) => {
+  try {
+    var collection = db.get('users');
+    var hashedPassword = await bcrypt.hash(req.body.password, 10);
+    collection.insert({
+      username: req.body.username,
+      email: req.body.email,
+      password_hash: hashedPassword,
+      name: {
+        first: req.body.firstName,
+        middle: req.body.MI,
+        last: req.body.lastName
+      },
+      phone_number: req.body.phone,
+      account_type: "user"
+    }, function(err, user) {
+      // something
+    });
+  } catch {
+    res.status(500).send();
+  }
+  res.redirect('/');
+});
 module.exports = router;
