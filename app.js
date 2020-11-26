04e6226558c6ff0518674ab3bb7acdb099fa8367
Jan-Felix Abellera <jfabellera@gmail.com>
2020-11-26 02:37:21 -0600

    adds landing page nav bar

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var methodOverride = require('method-override');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// var expensesRouter = require('./routes/expenses');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +22,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(methodOverride('_method'));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// app.use('/api/expenses', expensesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var monk = require('monk');
+// var db = monk('localhost:27017/billy');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/index');
 });
+
+router.get('/index', function(req, res) {
+  res.render('index', { title: 'Your personal expense tracker' });
+});
+
 
 module.exports = router;
