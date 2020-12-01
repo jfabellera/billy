e1b4b1881b3eb53684541ad07739c8e5199d4cbe
Jan-Felix Abellera <jfabellera@gmail.com>
2020-11-30 20:28:12 -0600

    adds user list view

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,27 @@
 var express = require('express');
+var bcrypt = require('bcrypt');
 var router = express.Router();
+var dotenv = require('dotenv');
+
+var monk = require('monk');
+dotenv.config();
+var db = monk('mongodb+srv://billy:'+process.env.mongodb_password+'@billy.ks9cj.mongodb.net/billy?retryWrites=true&w=majority');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  var collection = db.get('users');
   if(req.session.user && req.session.user.account_type == "admin") {
-    res.render('admin/users', { session: req.session });
+    collection.find({}, { sort: { name: 1 } }, (err, users) => {
+      if(err) throw err;
+      res.render('admin/users', { session: req.session, users: users });
+    });
   } else {
     res.status(401).send("Unauthorized");
   }
+});
+
+router.get('/:id', (req, res, next) => {
+
 });
 
 module.exports = router;
